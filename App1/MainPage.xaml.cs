9d6ec0224f064b09ed5a4fd6c696a043a28841cb
caramelopardalis <ltsu.koiwai@gmail.com>
2019-11-27 21:17:41 +0900

    Initial commit

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace App1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
